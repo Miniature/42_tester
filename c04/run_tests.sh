83e4a9f76efd894e8ead4b7b6db6a7d6ca9e8c51
William Davey <wdavey@F2N064.42adel.org.au>
2023-05-25 11:46:05 +0930

    add expected result for c04/ex04

--- c04/run_tests.sh
+++ c04/run_tests.sh
@@ -2,5 +2,5 @@ run_test ex00 ft_strlen.c '11,6,0'
 run_test ex01 ft_putstr.c 'hellohello world'
 run_test ex02 ft_putnbr.c '5-52147483647–2147483648-2147483647'
 run_test ex03 ft_atoi.c '44,-53,0,0,-5,2147483647,-2147483648'
-run_test ex04 ft_putnbr_base.c
+run_test ex04 ft_putnbr_base.c '500 -faa 7fffffff docm'
 run_test ex05 ft_atoi_base.c '555,-424,0,0,0,0'
--- c04/test_ex04.c
+++ c04/test_ex04.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 15:31:12 by wdavey            #+#    #+#             */
-/*   Updated: 2023/05/24 18:18:48 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/05/25 11:39:12 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ int	main(void)
 {
 	ualarm(100, 0);
 	ft_putnbr_base(500, "0123456789");
+	write(1, " ", 1);
 	ft_putnbr_base(-500, "abcdefghij");
+	write(1, " ", 1);
 	ft_putnbr_base(INT_MAX, "0123456789abcdef");
+	write(1, " ", 1);
 	ft_putnbr_base(345, "mrdoc");
 }
