c42840f16a38f8ead25b2265e8bc85fb906bf82f
William Davey <wdavey@F2N064.42adel.org.au>
2023-05-25 16:17:03 +0930

    isspace is important, improved c04/ex03 and c04/ex05 tests

--- c04/test_ex03.c
+++ c04/test_ex03.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 14:09:59 by wdavey            #+#    #+#             */
-/*   Updated: 2023/05/24 16:49:49 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/05/25 16:02:36 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,22 @@ int	main(void)
 {
 	char	buffer_6[100];
 	char	buffer_7[100];
+	char	buffer_8[100];
+	char	buffer_9[100];
 
 	sprintf(buffer_6, "%d", INT_MAX);
 	sprintf(buffer_7, "%d", INT_MIN);
+	sprintf(buffer_8, " \r \t   \f --++-+--%d", INT_MAX);
+	sprintf(buffer_9, "  \n  \v -+--+-%d", INT_MIN);
 	ualarm(100, 0);
-	printf("%i,%i,%i,%i,%i,%i,%i",
+	printf("%i,%i,%i,%i,%i,%i,%i,%i,%i",
 		ft_atoi("44"),
 		ft_atoi("-53"),
 		ft_atoi(NULL),
 		ft_atoi("–2147483648sdfsfs4534533"),
 		ft_atoi("     -+++-+-+-+---5dsd"),
 		ft_atoi(buffer_6),
-		ft_atoi(buffer_7));
+		ft_atoi(buffer_7),
+		ft_atoi(buffer_8),
+		ft_atoi(buffer_9));
 }
--- c04/test_ex05.c
+++ c04/test_ex05.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 15:37:58 by wdavey            #+#    #+#             */
-/*   Updated: 2023/05/25 11:57:37 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/05/25 16:12:12 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,13 @@ int	ft_atoi_base(char *str, char *base);
 int	main(void)
 {
 	ualarm(100, 0);
-	printf("%i,%i,%i,%i,%i,%i,%i",
+	printf("%i,%i,%i,%i,%i,%i,%i,%i",
 		ft_atoi_base("555", "0123456789"),
 		ft_atoi_base("-bccca", "abcd"),
 		ft_atoi_base(NULL, "abcdef"),
 		ft_atoi_base("348795", NULL),
 		ft_atoi_base("23894", "0"),
 		ft_atoi_base("sdfuids", "2346"),
-		ft_atoi_base("500", "0123454321"));
+		ft_atoi_base("500", "0123454321"),
+		ft_atoi_base("\r\v\t\n\f 5", "012345"));
 }
