49f669f504fad844b7c3a7fbc97a71bab63c616d
Mini <wndavey@hotmail.com>
2023-05-31 17:39:53 +0930

    c05/ex00/01: 0! = 1

--- c05/run_tests.sh
+++ c05/run_tests.sh
@@ -1,5 +1,5 @@
-test_function ex00 ft_iterative_factorial.c '1,0,0,6,1932053504'
-test_function ex01 ft_recursive_factorial.c '1,0,0,6,1932053504'
+test_function ex00 ft_iterative_factorial.c '1,1,0,6,1932053504'
+test_function ex01 ft_recursive_factorial.c '1,1,0,6,1932053504'
 test_function ex02 ft_iterative_power.c '1,0,1,16,0,0,1'
 test_function ex03 ft_recursive_power.c '1,0,1,16,0,0,1'
 test_function ex04 ft_fibonacci.c '55,1,0,-1'
