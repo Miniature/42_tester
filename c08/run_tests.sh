42b073f4c0a454b5eb1c73312186321d24ce4d4e
Mini <wndavey@hotmail.com>
2023-06-05 11:09:57 +0930

    c08/ex01 autotest doesnt support newlines :(

--- c08/run_tests.sh
+++ c08/run_tests.sh
@@ -1,5 +1,5 @@
 test_header ex00 'a'
-test_header ex01 '1,0,0,1,I have an even number of arguments.,I have an odd number of arguments.,0a'
+test_header ex01
 test_header ex02 '5,5'
 test_header ex03 '42,21'
 test_function ex04 'ft_strs_to_tab.c' 'a,1,c'
