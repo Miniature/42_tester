5dc37b4eccc1bc1dbc0fcd51ede7a632419d80ed
Mini <wndavey@hotmail.com>
2023-06-06 14:44:09 +0930

    c07/ex03 more customisable test

--- c07/run_tests.sh
+++ c07/run_tests.sh
@@ -2,6 +2,6 @@ norm
 test_function ex00 'ft_strdup.c' 'hello,'
 test_function ex01 'ft_range.c' '0,50,99,0x0'
 test_function ex02 'ft_ultimate_range.c' '100,0,50,99,0,0x0'
-test_function ex03 'ft_strjoin.c' 'a b c d e'
+test_function ex03 'ft_strjoin.c' 'abcdefg01abcdefg01abcdefg01abcdefg01abcdefg01abcdefg01abcdefg01abcdefg'
 test_function ex04 'ft_convert_base.c ft_convert_base2.c' '-ff'
 test_function ex05 'ft_split.c' 'abc,ghi,0x0'
--- c07/test_ex03.c
+++ c07/test_ex03.c
@@ -6,17 +6,42 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:07:19 by wdavey            #+#    #+#             */
-/*   Updated: 2023/05/31 14:12:23 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/06/06 14:43:21 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define STR_NUM 8
+#define STR_LEN 8
+#define SEP_LEN 2
 
 char	*ft_strjoin(int size, char **strs, char *sep);
 
 int	main(void)
 {
-	char	*strs[] = {"a", "b", "c", "d", "e"};
+	char	**strs;
+	char	*str;
+	char	*sep;
 
-	printf("%s", ft_strjoin(5, strs, " "));
+	strs = malloc(sizeof(char *) * 500);
+	for(int i = 0; i < STR_NUM; i++)
+	{
+		strs[i] = malloc(STR_LEN);
+		for(int j = 0; j < STR_LEN - 1; j++)
+		{
+			strs[i][j] = (j % 20) + 'a';
+		}
+		strs[i][STR_LEN - 1] = '\0';
+	}
+	sep = malloc(sizeof(char) * SEP_LEN);
+	for(int i = 0; i < SEP_LEN; i++)
+	{
+		sep[i] = (i % 20) + '0';
+	}
+	str = ft_strjoin(STR_NUM, strs, sep);
+	printf("%s", str);
+	free(str);
 }
