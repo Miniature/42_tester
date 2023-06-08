8ffdb3366b9a5c476c49223c6bd86635d81214ba
Mini <wndavey@hotmail.com>
2023-06-08 13:24:19 +0930

    c09/ex00 is meant to already include source files

--- c09/run_tests.sh
+++ c09/run_tests.sh
@@ -1,6 +1,5 @@
 norm
 printf "${ORANGE}${BOLD}Testing ex00...${NC}\n"
-`cp -R $THIS_DIR/$PROJECT_ID/srcs $PROJECT_PATH/ex00`
 (cd $PROJECT_PATH/ex00/srcs && sh ../libft_creator.sh && ar -t libft.a)
 printf "${ORANGE}${BOLD}No automatic test defined.${NC}\n\n"
 
